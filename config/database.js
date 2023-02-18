80fe6f30228360ec3257e85fa046548b3602bbfe
mwajs <m.wajs@cinema-city.pl>
2023-02-18 14:19:36 +0100

    added db port

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT,
   dialect: 'mysql'
 })
 
