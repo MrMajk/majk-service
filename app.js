1e23f415305f88493a62d7643a90325305060f8c
mwajs <m.wajs@cinema-city.pl>
2023-02-18 14:27:34 +0100

    added port

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ Reservation.belongsTo(Table)
     .sync({force: false, logging: console.log})
     .then(() => {
       console.log('DB started...')
-      app.listen($PORT || 8082)
+      app.listen(process.env.PORT || 8082)
     })
     .catch(error => {
       console.log(error)
